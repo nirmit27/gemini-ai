d24da13df5944a5bb71c69996c274148784e43e8
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-09-04 12:56:50 +0530

    image captioning

--- gemini_util.py
+++ gemini_util.py
@@ -1,11 +1,20 @@
 import os
+
+from PIL import Image
 import google.generativeai as genai
 
 # Loading the API key
 genai.configure(api_key=os.environ.get("API_KEY"))
 
 
-def load_gemini_pro() -> genai.GenerativeModel:
+def load_gemini_pro(model_name: str) -> genai.GenerativeModel:
     """Returns the Gemini Pro Generative model."""
-    model: genai.GenerativeModel = genai.GenerativeModel("gemini-pro")
+    model: genai.GenerativeModel = genai.GenerativeModel(model_name=model_name)
     return model
+
+
+def img_caption(prompt: str, image: Image.Image) -> str:
+    """Response for image captioning prompt."""
+    model: genai.GenerativeModel = load_gemini_pro("gemini-1.5-flash")
+    caption: str = model.generate_content([prompt, image]).text or ""
+    return caption
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+from PIL import Image
 
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-from gemini_util import load_gemini_pro
+from gemini_util import (load_gemini_pro, img_caption)
 
 work_dir: str = os.path.dirname(os.path.abspath(__file__))
 
@@ -20,12 +21,25 @@ st.set_page_config(
     layout="centered"
 )
 
-# Options menu
+# Options
 selected: str
-options: list[str] = ["ChatBot", "Image captioning", "Embed text", "Ask me anything"]
-icons: list[str] = ["chat-dots-fill", "image-fill", "textarea-t", "patch-question-fill"]
+options: list[str] = ["ChatBot", "Image captioning",
+                      "Embed text", "Ask me anything"]
+icons: list[str] = ["chat-dots-fill", "image-fill",
+                    "textarea-t", "patch-question-fill"]
 
-# Page layout
+# Page layout -
+
+
+def lb(n: int = 1) -> None:
+    """For generating line breaks."""
+    for _ in range(n):
+        st.markdown('''
+        <br>
+        ''', unsafe_allow_html=True)
+
+
+# Options sidebar
 with st.sidebar:
     selected = option_menu(
         menu_title="Gemini AI",
@@ -35,14 +49,17 @@ with st.sidebar:
         default_index=0
     )
 
+
+# ChatBot page
 if selected == "ChatBot":
-    model = load_gemini_pro()
+    # Loading the Gemini Pro Generative model
+    model = load_gemini_pro("gemini-pro")
 
     # Initialising chat session in Streamlit if not already active
     if "chat_session" not in st.session_state:
         st.session_state.chat_session = model.start_chat(history=[])
 
-    st.title("ChatBot 🤖")
+    st.title("🤖 ChatBot")
 
     # Displaying the chat history so far ...
     for message in st.session_state.chat_session.history:
@@ -55,6 +72,36 @@ if selected == "ChatBot":
         st.chat_message("user").markdown(user_prompt)
 
         # Displaying the response from Gemini
-        gemini_response: str = st.session_state.chat_session.send_message(user_prompt).text
+        gemini_response: str = st.session_state.chat_session.send_message(
+            user_prompt).text
         with st.chat_message("assistant"):
             st.markdown(gemini_response)
+
+
+# Image captioning page
+if selected == "Image captioning":
+    st.title("📸 Snap Narrate")
+
+    # Input image
+    image_upload = st.file_uploader("Generate a descriptive caption for your image.", type=[
+                                    ".jpg", ".jpeg", ".png"])
+
+    if st.button("Generate Caption"):
+        if image_upload:
+            image: Image.Image = Image.open(image_upload)
+
+            col1, col2 = st.columns(2, gap="medium")
+
+            with col1:
+                image.thumbnail((800, 600))
+                lb(1)
+                st.image(image)
+
+            default_prompt: str = "Write a short caption for this image."
+
+            # Fetching the response from Gemini Flash model
+            caption: str = img_caption(default_prompt, image)
+
+            with col2:
+                lb(1)
+                st.info(caption)
