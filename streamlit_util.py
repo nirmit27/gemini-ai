f7e8db09ef9b8f3a5047d2e1a5c892ba3ff8175b
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-09-08 11:11:30 +0530

    Update streamlit_util.py

--- streamlit_util.py
+++ streamlit_util.py
@@ -9,7 +9,7 @@ def lb(n: int = 1) -> None:
         ''', unsafe_allow_html=True)
 
 
-def page_header(title: str, color: str, size: float = 2.6, top: int = 30, bottom: int = 20) -> None:
+def page_header(title: str, color: str, size: float = 2.0, top: int = 30, bottom: int = 20) -> None:
     """For custom page headers/titles."""
     st.markdown(f"""
     <h1 style="text-align: center; color: {color}; font-size: {size}rem; margin-top: -{top}px;  margin-bottom: {bottom}px">
